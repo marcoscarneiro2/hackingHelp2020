3d3ca2f9716df19e7d6307574fdf47f8f15b5d86
Pedro Coutinho <coutinhopedro111@gmail.com>
2020-05-24 08:20:29 -0300

    Last Bugfix

--- client/src/components/layout/footer.vue
+++ client/src/components/layout/footer.vue
@@ -2,35 +2,33 @@
     <v-footer
     app
     fixed
-    padless
     color="#0296CC"
     height="125%"
     class="font-weight-medium"
         >
-        <v-col
-        class="text-center"
-        cols="4"
-        >
-            <v-btn class="mx-2" fab dark color="teal">
-                <v-icon dark>mdi-format-list-bulleted-square</v-icon>
-            </v-btn>
-        </v-col>
-        <v-col
+        <v-row align="center">
+            <v-col
             class="text-center"
             cols="4"
-        >
-            <v-btn class="mx-2" fab dark color="indigo">
-                <v-icon dark>fav-plus</v-icon>
-            </v-btn>
-        </v-col>
-        <v-col
-            class="text-center"
-            cols="4"
-        >
-            <v-btn class="mx-2" fab dark color="teal">
-                <v-icon dark>mdi-format-list-bulleted-square</v-icon>
-            </v-btn>
-        </v-col>
+            >
+                <v-btn class="mx-2 mb-5" fab dark color="teal">
+                </v-btn>
+            </v-col>
+            <v-col
+                class="text-center"
+                cols="4"
+            >
+                <v-btn class="mx-2 mb-5" fab dark color="indigo">
+                </v-btn>
+            </v-col>
+            <v-col
+                class="text-center"
+                cols="4"
+            >
+                <v-btn class="mx-2 mb-5" fab dark color="teal">
+                </v-btn>
+            </v-col>
+        </v-row>
     </v-footer>
 </template>
 
