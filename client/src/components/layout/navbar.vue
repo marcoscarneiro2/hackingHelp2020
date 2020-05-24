84ebc0c8e8bb6a8459a35d85c181cd6acb547e2a
Pedro Coutinho <coutinhopedro111@gmail.com>
2020-05-24 05:31:15 -0300

    Finalizado NavBar e Footer

--- client/src/components/layout/navbar.vue
+++ client/src/components/layout/navbar.vue
@@ -1,16 +1,29 @@
 <template>
 
-    <v-toolbar color="#0296CC" height="50%" prominent extended>
-      
+    <v-toolbar 
+    color="#0296CC" 
+    height="100%" 
+    >
+ 
+      <v-img 
+      class="mx-auto"
+      :src="logo"
+      alt=""
+      height="90%"
+      max-width="5%"
+      align-center
+      />
     </v-toolbar>
 
 </template>
 
 <script>
+import logo from "../../assets/LINGO.png"
 export default {
     name:"NavBar",
     data() {
       return {
+        logo
   
       }
     }
