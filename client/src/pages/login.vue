cb8b7295c648af32cce05daf070216d53bf8442a
Pedro Coutinho <coutinhopedro111@gmail.com>
2020-05-24 08:15:53 -0300

    Adding Lucas Pages

---
+++ client/src/pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+    <app id="inspire">
+        <v-col cols="12">
+            <v-row
+            justify-content="center"
+            align="center">
+                <v-card>
+                    oi
+                </v-card>
+            </v-row>
+        </v-col>
+
+    </app>
+</template>
+<script>
+export default {
+    data() {
+        
+    }
+}    
+</script>  
--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -2,7 +2,9 @@ import VueRouter from "vue-router";
 
 import Home from "../pages/home";
 import Local from "../pages/local";
-
+import Perfil from "../pages/Perfil";
+import Pesquisa from "../pages/pesquisa";
+import Login from "../pages/login";
 
 const router = new VueRouter({
     // rotas
@@ -20,6 +22,22 @@ const router = new VueRouter({
                 path:"/local",
                 name:"Local",
                 component:Local,
+                
+            },
+            {
+                path:"/perfil",
+                name:"Perfil",
+                component:Perfil,
+                
+            },{
+                path:"/pesquisa",
+                name:"Pesquisa",
+                component:Pesquisa,
+                
+            },{
+                path:"/login",
+                name:"Login",
+                component:Login,
                 
             }
         ]
